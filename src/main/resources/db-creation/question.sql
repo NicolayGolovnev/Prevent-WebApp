5c51ea64f04b3a366879362573ebd9377aa00e19
Nikita Ukolov <n.ukolllov@gmail.com>
2022-03-11 23:32:57 +0700

    создал файлы вставки записей опросников и их вопросов

---
+++ src/main/resources/db-creation/question.sql
@@ -0,0 +1,21 @@
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (1, 'Несварение желудка после еды', 1, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (2, 'Чрезмерная отрыжка, вздутие живота после еды', 2, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (3, 'Спазмы в желудке во время или после еды', 3, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (4, 'Ощущение, что еда просто осела в желудке, создавая ощущение
+дискомфорта, наполненности, распирания и вздутия вовремя или после еды', 4, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (5, 'Плохой привкус во рту', 5, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (6, 'Быстро насыщаетесь небольшим количеством еды', 6, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (7, 'Пропускаете еду или питаетесь с перебоями из-за отсутствия аппетита', 7, 1, 2);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (8, 'Сильные эмоции или мысли о еде', 1, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (9, 'Чувствуете голод через 1-2 часа после принятия большого количества пищи', 2, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (10, 'Боль в желудке, жжение и/или ноющая боль, через 1-4 часа после еды', 3, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (11, 'Боль в желудке, жжение и/или ноющая боль, которая облегчается приемом
+пищи, питьем газированной воды, сливок или молока или приемом
+антацидных препаратов', 4, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (12, 'Ощущение жжения в нижней части грудной клетки, особенно, если ложитесь
+или наклоняетесь вперед', 5, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (13, 'Проблемы с пищеварением устраняются отдыхом и расслаблением', 6, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (14, 'Принятие острой или жирной (жареной) пищи, шоколада, кофе, алкоголя
+вызывают жжение или ноющую боль в желудке', 7, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (15, 'Ощущение тошноты во время еды', 8, 1, 3);
+INSERT INTO public.question (id, content, num_question, weight_arg, id_quiz) VALUES (16, 'Трудность или боль во время проглатывания еды или жидкости', 9, 1, 3);
---
+++ src/main/resources/db-creation/quiz.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.quiz (id, title, access, gender, min_age, max_age, weight_arg, id_quiz) VALUES (1, 'Большой функциональный опросник PLS', true, 'all', 1, 100, 1, null);
+INSERT INTO public.quiz (id, title, access, gender, min_age, max_age, weight_arg, id_quiz) VALUES (2, 'Функция желудка', true, 'all', 1, 100, 1, 1);
+INSERT INTO public.quiz (id, title, access, gender, min_age, max_age, weight_arg, id_quiz) VALUES (3, 'Воспаление желудка', true, 'all', 1, 100, 1, 1);
