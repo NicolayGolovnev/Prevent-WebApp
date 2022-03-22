6a2439c7f4e3fe34e39dc767a2e47ab9e045a658
Nikolay Golovnev <kolya.golovnev@mail.ru>
2022-03-22 15:03:21 +0700

    Изменение базы данных + скрипт создания опросов

--- src/main/resources/db-creation/quiz.sql
+++ src/main/resources/db-creation/quiz.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.quiz (id, title, access, gender, min_age, max_age, weight_arg, id_quiz)
-VALUES (1, 'Большой функциональный опросник PLS', true, 'all', 1, 100, 1, null),
-       (2, 'Функция желудка', true, 'all', 1, 100, 1, 1),
-       (3, 'Воспаление желудка', true, 'all', 1, 100, 1, 1);
+INSERT INTO public.quiz (id, title, access, gender, min_age, max_age, weight_arg)
+VALUES (1, 'Большой функциональный опросник PLS', true, 'all', 1, 100, 1),
+       (2, 'Функция желудка', true, 'all', 1, 100, 1),
+       (3, 'Воспаление желудка', true, 'all', 1, 100, 1);
